62bf0716985fb0da63645691f240832a4acb79d7
gabrielLoboo <gabrielfrancisolobo@gmail.com>
2023-11-23 22:57:55 -0300

    responsivity update

--- src/app/(root)/components/Header/index.jsx
+++ src/app/(root)/components/Header/index.jsx
@@ -1,33 +1,33 @@
-'use client'
+"use client";
 
-import Container from '@/components/layout/Container'
-import Brand from '@/components/shared/Brand'
-import Button from '@/components/ui/Button'
-import React from 'react'
-import HeaderNavigation from './components/HeaderNavigation'
-import { RootHeaderNavigation } from '@/constants/RootHeaderNavigation'
-import { useRouter } from 'next/navigation'
+import Container from "@/components/layout/Container";
+import Brand from "@/components/shared/Brand";
+import Button from "@/components/ui/Button";
+import React from "react";
+import HeaderNavigation from "./components/HeaderNavigation";
+import { RootHeaderNavigation } from "@/constants/RootHeaderNavigation";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
-  const router = useRouter()
+  const router = useRouter();
 
   return (
-    <header className="w-full h-[90px]">
-      <Container className="flex items-center justify-between py-4">
-        <div className="w-[220px]">
+    <header className="2xl:w-full h-[90px]">
+      <Container className="flex flex-col lg:flex-row items-center justify-between py-4">
+        <div className="mb-4 sm:mb-0 sm:w-[220px]">
           <Brand />
         </div>
 
-        <div>
+        <div className="mb-4 sm:mb-0">
           <HeaderNavigation menu={RootHeaderNavigation} />
         </div>
 
         <div>
-          <Button onClick={() => router.push('/register')}>Começar</Button>
+          <Button onClick={() => router.push("/register")}>Começar</Button>
         </div>
       </Container>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
