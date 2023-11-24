5fcc991848a2a3422d13a1ebcda949790c5f3981
gabrielLoboo <gabrielfrancisolobo@gmail.com>
2023-11-24 02:54:44 -0300

    textarea fixed

--- src/app/(root)/sections/Hero/index.jsx
+++ src/app/(root)/sections/Hero/index.jsx
@@ -1,29 +1,29 @@
-'use client'
+"use client";
 
-import Container from '@/components/layout/Container'
-import Button from '@/components/ui/Button'
-import Text from '@/components/ui/Text'
-import Title from '@/components/ui/Title'
-import Image from 'next/image'
-import { useRouter } from 'next/navigation'
-import React from 'react'
-import { PiArrowRightBold } from 'react-icons/pi'
+import Container from "@/components/layout/Container";
+import Button from "@/components/ui/Button";
+import Text from "@/components/ui/Text";
+import Title from "@/components/ui/Title";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { PiArrowRightBold } from "react-icons/pi";
 
 const Hero = () => {
-  const router = useRouter()
+  const router = useRouter();
 
   return (
-    <section className=" min-h-[calc(100vh-180px)] pb-[250px]">
+    <section className="min-h-[calc(100vh-180px)] pb-[250px]">
       <Container>
-        <div className="flex justify-center gap-20">
-          <div className="flex flex-col gap-4 max-w-[700px] pt-[60px]">
+        <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center gap-4 pt-[60px]">
+          <div className="max-w-[700px] lg:w-1/2">
             <Title className="text-3xl font-semibold">
-              No Brasil, a falta de acesso e informação é a{' '}
-              <span className="underline  decoration-primary">principal</span>{' '}
+              No Brasil, a falta de acesso e informação é a{" "}
+              <span className="underline decoration-primary">principal</span>{" "}
               causa de morte em gestantes
               <span className="text-primary">.</span>
             </Title>
-            <Text className="text-xl ">
+            <Text className="text-xl">
               No cenário atual, enfrentamos uma triste realidade: a falta de
               acessibilidade está ceifando a vida de milhões de gestantes a cada
               ano. Diante desse desafio, nasceu uma startup determinada a
@@ -33,31 +33,31 @@ const Hero = () => {
 
             <div className="mt-10 max-w-[350px]">
               <Button
-                onClick={() => router.push('/register')}
+                onClick={() => router.push("/register")}
                 icon={PiArrowRightBold}
               >
                 Criar conta
               </Button>
             </div>
           </div>
-          <div className="flex relative justify-center items-center pr-20">
-            <div className="relative overflow-hidden rounded-[69%_31%_70%_30%_/_30%_54%_46%_70%] w-[500px] h-[500px]">
+          <div className="flex relative justify-center items-center pr-20 lg:w-1/2">
+            <div className="relative overflow-hidden rounded-[69%_31%_70%_30%_/_30%_54%_46%_70%] w-full h-[300px] lg:w-[500px] lg:h-[500px]">
               <Image
                 style={{
                   backgroundImage: `url(https://images.pexels.com/photos/618923/pexels-photo-618923.jpeg?auto=compress&cs=tinysrgb&w=1600)`,
-                  backgroundRepeat: 'no-repeat',
-                  backgroundPosition: 'center',
-                  backgroundSize: 'cover',
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
                 }}
                 fill={true}
               />
             </div>
-            <div className="absolute -z-10 left-2 top-2 rounded-[69%_31%_70%_30%_/_30%_54%_46%_70%] w-[500px] h-[500px] bg-primary" />
+            {/* <div className="absolute -z-10 left-2 top-2 rounded-[69%_31%_70%_30%_/_30%_54%_46%_70%] w-full h-[300px] lg:w-[500px] lg:h-[500px] bg-primary" /> */}
           </div>
         </div>
       </Container>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
